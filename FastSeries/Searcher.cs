c3da60f6cce98136774f8646db8885fa0ab7d9c2
wheel0012 <wheel0012@outlook.kr>
2019-07-15 21:02:17 +0900

    add searcher

---
+++ FastSeries/Searcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastSeries
+{
+    class Searcher
+    {
+        public List<Data> TimeSearch(List<Data> arr, Data value, int left, int right)
+        {
+            int middle = (left + right) / 2;
+            if (arr[middle].Time > value.Time)return TimeSearch(arr, value, left, right);
+
+            return null;
+        }
+    }
+}
